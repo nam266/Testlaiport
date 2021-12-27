81e704f66c1da940c8b4c0d64dcdf78c356fbb93
nam266 <dohoangnam1999@gmail.com>
2021-12-27 23:59:54 +0700

    .

--- app/db_connect.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from app.constants.constants import USER_DB
-
-cred = credentials.Certificate(USER_DB)
-firebase_admin.initialize_app(cred)
-db = firestore.client()
--- app/repositories/reported_post_repo.py
+++
@@ -1,6 +0,0 @@
-from app.db_connect import db
-
-reported_post_ref = db.collection(u'reported post')
-
-def save(report_post : dict):
-    reported_post_ref.document().set(report_post)
