31d411a67350e70668f80686eae0bd0e83c84c73
nam266 <dohoangnam1999@gmail.com>
2021-12-27 23:58:05 +0700

    .

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.routers import auth_router, post_router, comment_router
+from app.utils.exception_handler import custom_exception_handler
+from app.utils.cors_handler import handle_cors
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.include_router(auth_router.router, prefix= "/it4788")
+app.include_router(post_router.router, prefix= "/it4788")
+app.include_router(comment_router.router, prefix= "/it4788")
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
-
-@app.get("/")
-def home():
-    return {"message":"Hello TutLinks.com"}
+handle_cors(app)
+custom_exception_handler(app)
